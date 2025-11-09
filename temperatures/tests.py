70073cab21e42a7a7e23d124bc095284d7e9fe42
Guillermo <teres4ygu1lle@gmail.com>
2025-11-09 12:19:25 -0400

    Agregué autenticación por token, pruebas de API y configuración de .gitignore

--- temperatures/tests.py
+++ temperatures/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import CityTemperature
 
-# Create your tests here.
+class CityTemperatureModelTest(TestCase):
+    def test_create_city_temperature(self):
+        temp = CityTemperature.objects.create(
+            city="Cumaná",
+            temperature=29.5
+        )
+        self.assertEqual(temp.city, "Cumaná")
+        self.assertEqual(float(temp.temperature), 29.5)
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+class CityTemperatureAPITest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+
+    def test_create_temperature(self):
+        data = {"city": "Caracas", "temperature": 30.0}
+        response = self.client.post("/api/temperatures/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_temperatures(self):
+        response = self.client.get("/api/temperatures/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
--- weather_api/urls.py
+++ weather_api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token  
 from temperatures.views import CityTemperatureViewSet
 
 router = routers.DefaultRouter()
@@ -9,6 +10,7 @@ router.register(r'temperatures', CityTemperatureViewSet, basename='temperature')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),  
 ]
 
 
